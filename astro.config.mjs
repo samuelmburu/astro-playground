a7fdf8781ee98a016b7fa2d4cae5c418c814acd8
Samuel Kimama <->
2023-09-27 16:56:20 -0700

    add decap cms for testing, 1st push failed to build in prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
+// import remarkToc from 'remark-toc';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [preact()]
+    integrations: [preact()],
+    // Applied to .md and .mdx files
+    // remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeAccessibleEmojis],
 });
